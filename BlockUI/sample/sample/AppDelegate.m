bd0b62ffdc75f87fa794746e24b7498cde3e9e9f
lyt323.student <lyt323.student@sina.com>
2017-09-12 11:49:19 +0800

    优化代码

---
+++ BlockUI/sample/sample/AppDelegate.m
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.m
+//  sample
+//
+//  Created by 张玺 on 12-9-10.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+#import "BUIBlockUI.h"
+#import "MenuViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    
+    MenuViewController *menu = [[MenuViewController alloc] init];
+    menu.navigationItem.title = @"BlockUI";
+    
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:menu];
+    [self.window makeKeyAndVisible];
+
+    
+    
+    
+    //***************
+    [NSObject perform:^{
+        NSLog(@"start");
+        [NSThread sleepForTimeInterval:2];
+        NSLog(@"end");
+    } withCompletionHandler:^{
+        NSLog(@"finished");
+    }];
+    
+    
+    return YES;
+}
+
+
+@end
